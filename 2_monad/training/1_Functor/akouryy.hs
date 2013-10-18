982b1eb4dad254caf1af06f184ab79764c3f031f
akouryy <akouryy7@gmail.com>
2013-10-18 16:46:19 +0900

    solve 1_Functor

--- 2_monad/training/1_Functor/akouryy.hs
+++ 2_monad/training/1_Functor/akouryy.hs
@@ -1,7 +1,8 @@
+import Data.Function
 data X a =
     A (Int -> X a)
   | B a [Either Int a]
-  | C ((a -> Int) -> Int)
+  | C ((a -> a -> Int) -> Int)
 instance Functor X where
   -- fmap id = id
   -- fmap (f . g) = (fmap f) . (fmap g)
@@ -31,18 +32,25 @@ instance Functor X where
   --                      = ((fmap f) . (fmap g)) (B a p)
   fmap f (B a p) = B (f a) (fmap (fmap f) p)
 
+  -- (f `on` g) x y = f (g x) (g y)
+  -- fmap :: (a -> b) -> C ((a -> a -> Int) -> Int) -> C ((b -> b -> Int) -> Int)
+  fmap f (C p) = C (\h -> p (h `on` f))
+  -- fmap f (C p) = C (\h -> p (\i -> ((h . f) i) . f))
+  -- fmap f (C p) = C ((\h :: b -> b -> Int) -> p ((\i :: a) -> ((h . f :: a -> b -> Int) i :: b -> Int) . f :: a -> Int) :: Int)
+
+{-
   -- fmap :: (a -> b) -> C ((a -> Int) -> Int) -> C ((b -> Int) -> Int)
   -- fmap id (C p) = C (\h -> p (h . id))
   --               = C (\h -> p h)
   --               = C p
   -- fmap (f . g) (C p) = C (\h -> p (h . (f . g)))
   --                    = C (\h -> p (h . f . g))
-  --                    = C (\h -> p ((h . f) . g)) 
-  --                    ?????
+  --                    = C (\h -> p ((h . f) . g))
   --                    = C (\h -> (\i -> p (i . g)) (h . f))
   --                    = fmap f (C (\i -> p (i . g)))
   --                    = fmap f (fmap g (C p))
   --                    = ((fmap f) . (fmap g)) (C p)
   fmap f (C p) = C (\h -> p (h . f))
   -- fmap f (C p) = C ((\h :: b -> Int) -> p (h . f :: a -> Int) :: Int)
--- main = print "OK"
+-}
+main = print "OK"
