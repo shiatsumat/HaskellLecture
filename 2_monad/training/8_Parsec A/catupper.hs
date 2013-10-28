184925c8bfdbc096a7a1b4035c4c7338cb80d12a
Ken Ogura <nekarugo628@gmail.com>
2013-10-28 19:36:15 +0900

    fix spaces

--- 2_monad/training/8_Parsec A/catupper.hs
+++ 2_monad/training/8_Parsec A/catupper.hs
@@ -3,10 +3,10 @@ import Text.Parsec.String
 import Text.Parsec.Expr
 import Control.Applicative((<$>), (<*>))
 number :: Parser Double
-number = do spaces
-            nat   <- many1 digit
+number = do nat   <- many1 digit
             point <- option "" $ (:) <$> char '.' <*> many1 digit
             exp   <- option "" $ (:) <$> char 'e' <*> ((++) <$> option "" (string "-") <*>  (many1 digit))
+            spaces
             return $ read $ nat++point++exp
 {- edit here -}
 
@@ -16,18 +16,17 @@ expression = spaces >> buildExpressionParser operators terms
 {- edit here -}
 
 operators = [
-                [Prefix (spaces >> (char '-') >> return (* (-1))) , Prefix (spaces >> char '+' >> return (* 1))],
-                [Infix  (spaces >> (char '*') >> return (*)) AssocLeft, Infix (spaces >> (char '/') >> return (/)) AssocLeft],
-                [Infix  (spaces >> (char '+') >> return (+)) AssocLeft, Infix (spaces >> (char '-') >>  return (-)) AssocLeft],
-                [Infix (spaces >> char '^' >> return(**)) AssocRight]
+                [Prefix (char '-' >> spaces >> return (* (-1))) , Prefix (char '+' >> spaces >> return (* 1))],
+                [Infix  (char '*' >> spaces >> return (*)) AssocLeft, Infix (char '/' >> spaces >> return (/)) AssocLeft],
+                [Infix  (char '+' >> spaces >> return (+)) AssocLeft, Infix (char '-' >>  spaces >> return (-)) AssocLeft],
+                [Infix ( char '^' >> spaces >> return(**)) AssocRight]
             ]
 
 terms :: Parser Double
 terms = number <|> between (char '(') (char ')') expression <|> Main.log
 
 log :: Parser Double
-log = do spaces
-         string "log"
+log = do string "log"
          spaces
          char '('
          spaces
@@ -38,6 +37,7 @@ log = do spaces
          b <- expression
          spaces
          char ')'
+         spaces
          return $ logBase a b
 
 main = do s <- getContents
